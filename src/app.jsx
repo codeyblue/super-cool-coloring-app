69e698323d20d09bcaef8e9ab3c024b5336e96ab
Alexandra <alexdebish@gmail.com>
2023-10-13 11:53:32 -0400

    replace grommet with chakra and get the foundation of chakra prepared in the ui

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 import { Upload } from './upload.jsx';
 import { Draw } from './draw.jsx';
@@ -9,6 +10,14 @@ import { withAppState, useAppState } from './state';
 import { withDB } from './state-db.jsx';
 
 import './app.css';
+
+const theme = extendTheme({
+  colors: {
+    primary: {
+      100: '#FFC2E2'
+    }
+  }
+});
 
 const App = withDB(withAppState(() => {
   const { route } = useAppState();
@@ -25,9 +34,10 @@ const App = withDB(withAppState(() => {
   }
 }));
 
-
 ReactDOM.createRoot(
   document.getElementById('root')
 ).render(
-  <App />
+  <ChakraProvider theme={theme}>
+    <App />
+  </ChakraProvider>
 );
--- src/draw.jsx
+++ src/draw.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Button } from 'grommet';
+import { Box, Grid, GridItem, IconButton } from '@chakra-ui/react';
 import Color from 'color';
 import { FormRefresh } from 'grommet-icons';
 
@@ -12,19 +12,19 @@ export const Draw = () => {
     resetApp,
   } = useAppState();
 
+  const sidebarWidth = '50px';
+  const bottombarHeight = '100px';
+
   return (
-    <div style={{
-      '--sidebar-size': '50px',
-      '--bottombar-size': '100px',
-      display: 'grid',
-      width: '100%',
-      height: '100%',
-      justifyItems: 'stretch',
-      gridTemplateColumns: 'calc(100% - var(--sidebar-size)) var(--sidebar-size)',
-      gridTemplateRows: 'calc(100% - var(--bottombar-size)) var(--bottombar-size)',
-      background: '#ffffff'
-    }}>
-      <div style={{
+    <Grid
+      templateAreas={`'canvas sidebar'
+                      'bottombar bottombar'`}
+      gridTemplateColumns={`1fr ${sidebarWidth}`}
+      gridAutoRows={`1fr ${bottombarHeight}`}
+      gap="0"
+      h="100%"
+    >
+      <GridItem area="canvas" style={{
         overflow: 'hidden',
         borderRight: `1px solid ${Color('#EAE8FF').darken(.1).hex()}`,
         borderBottom: `1px solid ${Color('#EAE8FF').darken(.1).hex()}`,
@@ -32,17 +32,47 @@ export const Draw = () => {
         borderRadius: '0 0 10px 0'
       }}>
         <Canvas />
-      </div>
+      </GridItem>
 
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between'
-      }}>
+      <GridItem area="sidebar">
         <Box id="file-controls">
-          <Button onClick={resetApp} icon={<FormRefresh />} />
+          <IconButton
+            aria-label='Reset App'
+            onClick={resetApp}
+            icon={<FormRefresh />}
+          />
+          <IconButton
+            aria-label='Reset App'
+            onClick={resetApp}
+            icon={<FormRefresh />}
+          />
+          <IconButton
+            aria-label='Reset App'
+            onClick={resetApp}
+            icon={<FormRefresh />}
+          />
         </Box>
-      </div>
-    </div>
+      </GridItem>
+
+      <GridItem area="bottombar">
+        <Box id="file-controls">
+          <IconButton
+            aria-label='Reset App'
+            onClick={resetApp}
+            icon={<FormRefresh />}
+          />
+          <IconButton
+            aria-label='Reset App'
+            onClick={resetApp}
+            icon={<FormRefresh />}
+          />
+          <IconButton
+            aria-label='Reset App'
+            onClick={resetApp}
+            icon={<FormRefresh />}
+          />
+        </Box>
+      </GridItem>
+    </Grid>
   );
 };
